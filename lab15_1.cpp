cb1c4174f14f287e90edff8042f0426d63efb403
Warngkana <warangkana_chit@cmu.ac.th>
2024-02-02 14:23:17 +0700

    finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,37 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
+    
+	double a = 5;
+	char b = 'A';
+	char &c = b;
+	cout << a << " " << b << " " << c;
 	
+	double *x = &a;
+	char *y = &b;
+	double **z = &x;
+	cout << " " << &x << " " << &y << " " << &z;
+	
+	cout << "\n" << &a << " " << &y << " " << &y;
+	cout << " " << &x << " " << &y << " " << &z;
+	
+	b = c+5;
+	cout << "\n" << a << " " << b << " " << c;
+	cout << " " << &x << " " << &y << " " << &z;
+	
+	b = *y+17;
+	cout << "\n" << a << " " << b << " " << c;
+	cout << " " << &x << " " << &y << " " << &z;
+	
+	int n = 1;
+	a = (*x)+1;
+	cout << "\n" << a << " " << b << " " << c;
+	cout << " " << &x << " " << &y << " " << &z;
+	
+	a = (**z)+n;
+	cout << "\n" << a << " " << b << " " << c;
+	cout << " " << &x << " " << &y << " " << &z;
 	
 	return 0;
 }
